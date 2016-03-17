39475cb06218bbc4f5490983e549a77fffe39efb
bobanko <mishko.vladimir@gmail.com>
2016-03-17 20:50:26 +0200

    autostart reverted

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ restart.addEventListener('click', reload);
 
 
 //start game!111
-matrixData();
+//matrixData();
 
 function random(min, max) {
 	return Math.floor(Math.random() * (max - min + 1)) + min;
